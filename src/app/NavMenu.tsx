eab2adc743eb78af05b2d5de8e96aa83a4192d3f
Mello Birkan <mgbirkan@icloud.com>
2023-09-08 01:39:05 -0300

    first commit

---
+++ src/app/NavMenu.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavMenu() {
+  return (
+    <nav className=" bg-blue-600 flex justify-between p-6">
+      <Link href={"/"}>
+        <Image src="/logo.svg" width={216} height={30} alt="NextSapce Logo" />
+      </Link>
+      <ul className="flex gap-4 text-blue-50">
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/users"}>Users</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
